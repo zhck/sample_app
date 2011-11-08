5d792eddd9023bd29ec7fd0b6ee0ef31cbcd92d7
zhck <zhck@mail.ru>
2011-11-08 15:47:11 +0200

    improve spec test2

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -33,5 +33,19 @@ describe PagesController do
                           @base_title + "Contact")
     end
   end
+  
+   describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          @base_title + "About")
+    end
+  end
 
 end
